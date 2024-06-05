1b351d4322b3b74d1927469b911086f36c3d1cd3
asoupsandwich <shanebot90@gmail.com>
2024-06-04 21:24:57 -0500

    v0.2

--- scripts/analyst_jobs_project.sql
+++ scripts/analyst_jobs_project.sql
@@ -19,25 +19,96 @@
 -- ExxonMobil
 
 -- 3.	How many postings are in Tennessee? How many are there in either Tennessee or Kentucky?
+-- SELECT *
+-- FROM data_analyst_jobs
+-- WHERE location = 'TN' [OR location = 'KY']
 
+--21 in Tennessee, 27 in TN or KY
 
 -- 4.	How many postings in Tennessee have a star rating above 4?
+-- SELECT *
+-- FROM data_analyst_jobs
+-- WHERE location = 'TN' AND star_rating > 4
+
+-- 3 postings
 
 -- 5.	How many postings in the dataset have a review count between 500 and 1000?
+-- SELECT *
+-- FROM data_analyst_jobs
+-- WHERE review_count BETWEEN 500 AND 1000
+
+-- 151 postings
 
 -- 6.	Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating?
 
+-- SELECT 
+-- 	location AS state, 
+-- 	AVG(star_rating) AS avg_rating
+-- FROM 
+-- 	data_analyst_jobs
+-- GROUP BY 
+-- 	state
+-- ORDER BY 
+-- 	avg_rating DESC;
+
+-- NE shows the highest average rating
+
 -- 7.	Select unique job titles from the data_analyst_jobs table. How many are there?
 
+-- SELECT DISTINCT(title)
+-- FROM data_analyst_jobs;
+
+-- 881 unique titles.
+	
 -- 8.	How many unique job titles are there for California companies?
+
+-- SELECT DISTINCT(title)
+-- FROM data_analyst_jobs
+-- WHERE location = 'CA'
+
+-- 230 unique titles in Cali
 
 -- 9.	Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
 
+-- SELECT company, AVG(star_rating) AS avg_rating
+-- FROM data_analyst_jobs
+-- WHERE review_count > 5000
+-- GROUP BY company
+
+-- 41 total companies with over 5000 reviews.
+
 -- 10.	Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
 
+-- SELECT company, AVG(star_rating) AS avg_rating
+-- FROM data_analyst_jobs
+-- WHERE review_count > 5000
+-- GROUP BY company
+-- ORDER BY avg_rating DESC
+
+-- General Motors holds an average rating of 4.19 stars.
+	
 -- 11.	Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 
 
+-- SELECT DISTINCT(title)
+-- FROM data_analyst_jobs
+-- WHERE title LIKE '%Analyst%'
+
+-- 754 distinct titles containing the word 'Analyst'
+
 -- 12.	How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
+
+-- SELECT DISTINCT(title)
+-- FROM 
+-- 	data_analyst_jobs
+-- WHERE 
+-- 	title NOT LIKE '%Analyst%'
+-- 	AND title NOT LIKE '%Analytics%' 
+-- 	AND title NOT LIKE '%ANALYST%' 
+-- 	AND title NOT LIKE '%analyst%' 
+-- 	AND title NOT LIKE '%analytics%'	
+-- 	AND title NOT LIKE '%ANALYTICS%'
+
+-- the remaining 4 title all have tableau in common
 
 -- **BONUS:**
 -- You want to understand which jobs requiring SQL are hard to fill. Find the number of jobs by industry (domain) that require SQL and have been posted longer than 3 weeks. 
@@ -45,3 +116,30 @@
 --  - Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top. 
 --   - Which three industries are in the top 4 on this list? How many jobs have been listed for more than 3 weeks for each of the top 4?
 
+-- SELECT 
+-- 	domain,
+-- 	COUNT(days_since_posting)
+-- FROM 
+-- 	data_analyst_jobs
+-- WHERE 
+-- 	domain IS NOT NULL
+-- 	AND skill ILIKE '%SQL%'
+-- 	AND days_since_posting > 21
+-- GROUP BY 
+-- 	domain
+-- ORDER BY count DESC
+
+-- "Internet and Software"
+-- "Banks and Financial Services"
+-- "Consulting and Business Services"
+-- "Health Care"
+
+
+
+
+--- 
+-- SELECT *
+-- FROM data_analyst_jobs
+-- WHERE domain = 'Banks and Financial Services'
+-- 	AND skill ILIKE '%SQL%'
+-- 	ORDER BY days_since_posting DESC
